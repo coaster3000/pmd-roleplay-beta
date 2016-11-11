7bf5a1534d33795c92e2a55e2461ee7dc2356519
Christopher Krier <ckrier.3000@gmail.com>
2016-11-11 17:20:48 -0500

    Create base script files.

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+	var defaultTasks = ['concat'/*, 'uglify'*/]; // used for watch as well
+
+	var files = [ 'src/UserScriptHeader.js', 'src/Header.js',
+				'src/Main.js', 'src/Footer.js' ];
+
+	var opkg = grunt.file.readJSON('package.json');
+	grunt.initConfig({
+		pkg: opkg,
+		
+		concat: {
+			options: {
+				process: function(src, filepath) {
+					return (filepath != "src/UserScriptHeader.js"?'\n// Source: ' + filepath + '\n\n' : "") + src.replace(/@VERSION@/g, opkg.version);
+				}
+			},
+			all: {
+				src: files,
+				dest: 'dist/<%= pkg.name %>.user.js',
+			}
+		},
+
+		/*uglify: {
+			all: {
+				files: {
+					'pmd-roleplay-extended-chat.min.user.js': ['pmd-roleplay-extended-chat.user.js']
+				}
+			}
+		},*/
+
+		watch: {
+			all: {
+				files: files,
+				tasks: defaultTasks,
+				options: {
+					debounceDelay: 250
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', defaultTasks);
+};
---
+++ src/UserScriptHeader.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name	pmd-rp-beta
+// @namespace	coaster3000@pmd-roleplay.forumotion.com
+// @description	Provides the necessary userscripts to test out features for the site. These are scripts that will be placed on the site.
+// @include	http://pmd-roleplay.forumotion.org/*
+// @include	https://pmd-roleplay.forumotion.org/*
+// @match	https://pmd-roleplay.forumotion.org/*
+// @match	http://pmd-roleplay.foruomtion.org/*
+// @version	@VERSION@
+// @grant	none
+// ==/UserScript==
