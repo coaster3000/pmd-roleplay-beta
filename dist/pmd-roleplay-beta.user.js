27a05876d413f31271ce3d074fc84008a3149990
Christopher Krier <ckrier.3000@gmail.com>
2017-08-11 01:02:00 -0400

    Update to 0.0.5
    Switched distribution urls to openuserjs.org

--- dist/pmd-roleplay-beta.user.js
+++ dist/pmd-roleplay-beta.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name	pmd-rp-beta
 // @namespace	coaster3000@pmd-roleplay.forumotion.com
-// @description	A userscript that provides all the custom new site features that are being tested. Once fully tested, these features are removed from this script and added to the site for global use.
+// @description	A userscript that provides beta features for the site pmd-roleplay.forumotion.org. All beta features are eventually added to site once enough feedback is provided on site from userscript testing.
 // @include	http://pmd-roleplay.forumotion.org/*
 // @include	https://pmd-roleplay.forumotion.org/*
 // @match	https://pmd-roleplay.forumotion.org/*
 // @match	http://pmd-roleplay.forumotion.org/*
-// @version	0.0.4
-// @updateURL https://github.com/coaster3000/pmd-roleplay-beta/raw/releases/dist/pmd-roleplay-beta.meta.js
-// @downloadURL https://github.com/coaster3000/pmd-roleplay-beta/raw/releases/dist/pmd-roleplay-beta.user.js
+// @version	0.0.5
+// @license MIT; https://github.com/coaster3000/pmd-roleplay-beta/blob/master/LICENSE.md
+// @supportURL https://github.com/coaster3000/pmd-roleplay-beta/issues
+// @updateURL https://openuserjs.org/meta/coaster3000/pmd-rp-beta.meta.js
+// @downloadURL https://openuserjs.org/install/coaster3000/pmd-rp-beta.user.js
 // @grant	none
 // ==/UserScript==
 
--- src/UserScriptHeader.js
+++ src/UserScriptHeader.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name	pmd-rp-beta
 // @namespace	coaster3000@pmd-roleplay.forumotion.com
-// @description	A userscript that provides all the custom new site features that are being tested. Once fully tested, these features are removed from this script and added to the site for global use.
+// @description	A userscript that provides beta features for the site pmd-roleplay.forumotion.org. All beta features are eventually added to site once enough feedback is provided on site from userscript testing.
 // @include	http://pmd-roleplay.forumotion.org/*
 // @include	https://pmd-roleplay.forumotion.org/*
 // @match	https://pmd-roleplay.forumotion.org/*
 // @match	http://pmd-roleplay.forumotion.org/*
 // @version	@VERSION@
-// @license https://github.com/coaster3000/pmd-roleplay-beta/blob/releases/LICENSE.md
+// @license MIT; https://github.com/coaster3000/pmd-roleplay-beta/blob/master/LICENSE.md
 // @supportURL https://github.com/coaster3000/pmd-roleplay-beta/issues
-// @updateURL https://github.com/coaster3000/pmd-roleplay-beta/raw/releases/dist/pmd-roleplay-beta.meta.js
-// @downloadURL https://github.com/coaster3000/pmd-roleplay-beta/raw/releases/dist/pmd-roleplay-beta.user.js
+// @updateURL https://openuserjs.org/meta/coaster3000/pmd-rp-beta.meta.js
+// @downloadURL https://openuserjs.org/install/coaster3000/pmd-rp-beta.user.js
 // @grant	none
 // ==/UserScript==
